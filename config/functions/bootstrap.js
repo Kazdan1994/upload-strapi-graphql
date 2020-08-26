5a57409b75ce25442a748a78670e3efb9b67e05c
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-08-26 17:19:17 +0200

    feat(server): add graphql upload

---
+++ config/functions/bootstrap.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * An asynchronous bootstrap function that runs before
+ * your application gets started.
+ *
+ * This gives you an opportunity to set up your data model,
+ * run jobs, or perform some special logic.
+ *
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
+ */
+
+module.exports = async () => {
+
+  await strapi.plugins['users-permissions'].services.user.add({
+    id: 1,
+    username: 'John Doe',
+    email: 'john@doe.fr',
+    provider: 'local',
+    password: '$2a$10$jA6p3IBpqc6hSco8AYNDhORpAdP6yd/bZUmAZNhLFJjs/O//LEOVq',
+    resetPasswordToken: null,
+    confirmed: 1,
+    blocked: 0,
+    role: 1,
+    veterinary: 1,
+    created_by: 1,
+    updated_by: 1
+  })
+
+  await strapi.services.veterinary.create({
+    id: 1,
+    ordinal: 10775,
+    user: 1,
+    created_by: 1,
+    updated_by: 1
+  })
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'da5986be-59a6-493a-bc49-354217ba54dc'
+};
